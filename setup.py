473a4959701d1983db2515cd65ad60d15923a8fd
philloooo <phillis.tt@gmail.com>
2018-01-25 14:22:43 -0600

    feat(version): get version from tag

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
+from subprocess import check_output
+
+
+def get_version():
+    try:
+        tag = check_output([
+            'git', 'describe',  '--tags', '--abbrev=0', '--match=[0-9]*'])
+        return tag.strip('\n')
+    except:
+        # if somehow you get the repo not from git,
+        # hardcode default major.minor version
+        return '1.2.0'
+
 
 setup(
     name='dictionaryutils',
-    version='0.0.0',
+    version=get_version(),
     packages=find_packages(),
     install_requires=[
         'PyYAML==3.11',
