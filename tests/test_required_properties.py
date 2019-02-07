7e00e813cad02182317f706f411002f170fbf8c7
Viktorija Zaksas <vzpgb@uchicago.edu>
2019-02-07 12:21:53 -0600

    fix(required): Updated tests to catch absence of required section in schema

--- tests/test_required_properties.py
+++ tests/test_required_properties.py
@@ -4,6 +4,8 @@ from dictionaryutils import dictionary
 def test_required_program_property():
     required = "dbgap_accession_number"
     schema = dictionary.schema["program"]
+    assert "required" in schema, (
+                "No required properties defined in a program node")
     assert required in schema["required"], (
                 "{} is required property for a program node, but not defined "
                 "as required".format(required))
@@ -12,15 +14,20 @@ def test_required_program_property():
 def test_required_project_property():
     required = "dbgap_accession_number"
     schema = dictionary.schema["project"]
+    assert "required" in schema, (
+                "No required properties defined in a project node")
     assert required in schema["required"], (
                 "{} is required property for a project node, but not defined "
                 "as required".format(required))
 
 
-def test_required_properties():
+def test_required_ubiquitous_properties():
     required = ["submitter_id", "type"]
     for schema in dictionary.schema.values():
         if not schema["id"] in ("program", "project"):
+            assert "required" in schema, (
+                    "No required properties defined in a {} "
+                    "node".format(schema["id"]))
             for property in required:
                 assert property in schema["required"], (
                     "{} is required property for a {} node, but not defined "
